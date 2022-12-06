61fe24d7c06fd8bf1150caa02b21983a6053890a
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-12-06 01:39:34 +0100

    Add junit testing framework

---
+++ src/test/java/org/quydusaigon/predatorsim/Tests.java
@@ -0,0 +1,13 @@
+package org.quydusaigon.predatorsim;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+
+    @Test
+    void onePlusOneIsTwo() {
+        assertEquals(2, 1 + 1);
+    }
+}
