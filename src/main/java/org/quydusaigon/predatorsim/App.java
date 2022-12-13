ebd273d7e0c0b29f452fdd934346960b2b01c7d2
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2022-12-14 00:38:58 +0100

    Testing app

--- src/main/java/org/quydusaigon/predatorsim/App.java
+++ src/main/java/org/quydusaigon/predatorsim/App.java
@@ -5,9 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.quydusaigon.predatorsim.gameengine.*;
-
+import org.quydusaigon.predatorsim.gameengine.components.Sprite;
+import org.quydusaigon.predatorsim.gameengine.components.Transform;
 import java.io.IOException;
 
 /**
@@ -22,12 +24,24 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        GameObject a = new GameObject(new Animal());
-        a.start();
-        a.getComponent(Animal.class)
-                .orElseThrow().stateConstructor.wanderingState.test();
+        org.quydusaigon.predatorsim.gameengine.Scene scene1 = new org.quydusaigon.predatorsim.gameengine.Scene();
 
-        stage.show();
+        Transform transform1 = new Transform(100, 100);
+        Transform transform2 = new Transform(200, 200);
+
+        GameObject obj1 = new GameObject(transform1);
+        GameObject obj2 = new GameObject(transform2);
+
+        Image image1 = new Image("D:\\My CSE\\Programming\\Java OOP\\predatorsim\\src\\main\\resources\\org\\quydusaigon\\predatorsim\\images\\animal\\prey.jpg");
+        Image image2 = new Image("D:\\My CSE\\Programming\\Java OOP\\predatorsim\\src\\main\\resources\\org\\quydusaigon\\predatorsim\\images\\animal\\predator.jpg");
+
+        Sprite sprite1 = new Sprite(image1);
+        Sprite sprite2 = new Sprite(image2);
+
+        obj1.addComponent(sprite1);
+        obj2.addComponent(sprite2);
+
+
     }
 
     public static void setRoot(String fxml) throws IOException {
