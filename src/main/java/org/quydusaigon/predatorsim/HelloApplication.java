c3bf00e4c7cbf580f7488174bd3cc85a0415a153
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2022-12-14 00:04:06 +0100

    Testing with Rendering

--- src/main/java/org/quydusaigon/predatorsim/HelloApplication.java
+++ src/main/java/org/quydusaigon/predatorsim/HelloApplication.java
@@ -4,77 +4,37 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Point2D;
 import javafx.scene.paint.ImagePattern;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
+import org.quydusaigon.predatorsim.gameengine.GameObject;
+import org.quydusaigon.predatorsim.gameengine.Scene;
+import org.quydusaigon.predatorsim.gameengine.components.Sprite;
+import org.quydusaigon.predatorsim.gameengine.components.Transform;
 
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+        Scene scene1 = new Scene();
 
+        Transform transform1 = new Transform(100, 100);
+        Transform transform2 = new Transform(200, 200);
 
+        GameObject obj1 = new GameObject(transform1);
+        GameObject obj2 = new GameObject(transform2);
 
-        Rectangle rect = new Rectangle(30, 30);
-        Ellipse elli = new Ellipse(30, 30);
+        Image image1 = new Image("D:\\My CSE\\Programming\\Java OOP\\predatorsim\\src\\main\\resources\\org\\quydusaigon\\predatorsim\\images\\animal\\prey.jpg");
+        Image image2 = new Image("D:\\My CSE\\Programming\\Java OOP\\predatorsim\\src\\main\\resources\\org\\quydusaigon\\predatorsim\\images\\animal\\predator.jpg");
 
-        elli.setCenterX(300);
-        elli.setCenterY(300);
-        rect.setX(500);
-        rect.setY(500);
+        Sprite sprite1 = new Sprite(image1);
+        Sprite sprite2 = new Sprite(image2);
 
-        primaryStage.setTitle("Testing");
-        primaryStage.setWidth(1000);
-        primaryStage.setHeight(600);
-        Image icon = new Image("D:\\demo\\src\\predator.jpg");
-        primaryStage.getIcons().add(icon);
-
-
-        int speed = 5;
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long l) {
-                double vectorX = (elli.getCenterX() - rect.getX())/100.0;
-                double vectorY = (elli.getCenterY() - rect.getY())/100.0;
-                Point2D p = new Point2D(vectorX, vectorY);
-                p = p.normalize().multiply(speed);
-
-                double recNewX = rect.getX() + p.getX();
-                double recNewY = rect.getY() + p.getY();
-                if (recNewX < 0) {
-                    recNewX = 0;
-                }
-                if (recNewY < 0) {
-                    recNewY = 0;
-                }
-                rect.setX(recNewX);
-                rect.setY(recNewY);
-
-                double elliNewX = elli.getCenterX() + p.getX();
-                double elliNewY = elli.getCenterY() + p.getY();
-                if (elliNewX < 0) {
-                    elliNewX = 0;
-                }
-                if (elliNewY < 0) {
-                    elliNewY = 0;
-                }
-                elli.setCenterX(elliNewX);
-                elli.setCenterY(elliNewY);
-
-            }
-        };
-        timer.start();
-
-        Pane root = new Pane(rect, elli);
-        Scene scene = new Scene(root, Color.BEIGE);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        obj1.addComponent(sprite1);
 
     }
 
