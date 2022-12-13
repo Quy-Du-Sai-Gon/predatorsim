a90c8f60bb32c076d2e09bab62fad35d677e9542
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2022-12-13 20:07:47 +0100

    Added Scene class

---
+++ src/main/java/org/quydusaigon/predatorsim/gameengine/Scene.java
@@ -0,0 +1,30 @@
+package org.quydusaigon.predatorsim.gameengine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scene {
+
+    private final List<GameObject> gameObjects = new ArrayList<>();
+
+    public void start() {
+        for (GameObject gameObject : gameObjects) {
+            gameObject.start();
+        }
+    }
+
+    public void update() {
+        // Update
+        for (GameObject gameObject : gameObjects) {
+            gameObject.update();
+        }
+        // Render
+
+        // Collision
+    }
+
+    public void addGameObject(GameObject newGameObject) {
+        gameObjects.add(newGameObject);
+    }
+
+}
