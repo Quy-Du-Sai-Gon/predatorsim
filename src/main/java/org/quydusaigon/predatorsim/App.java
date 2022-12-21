dfce7618c9fa40168c5e7e3417b0b7a26eb06a47
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-12-22 00:46:55 +0100

    Update base scene

--- src/main/java/org/quydusaigon/predatorsim/App.java
+++ src/main/java/org/quydusaigon/predatorsim/App.java
@@ -2,9 +2,12 @@ package org.quydusaigon.predatorsim;
 
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import org.quydusaigon.predatorsim.util.Prefabs;
 
 /**
  * JavaFX App
@@ -15,6 +18,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        stage.setTitle("Collision detection demo");
+        stage.setWidth(1000);
+        stage.setHeight(800);
+
+        root = Prefabs.ROOT();
+        var scene = new Scene(root);
+
+        stage.setScene(scene);
         stage.show();
     }
 
