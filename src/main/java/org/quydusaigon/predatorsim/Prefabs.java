682bb111cf0facea189e445d75502fa3aab68f1c
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-12-22 00:32:16 +0100

    Add ROOT to Prefabs for root GameObject creation

--- src/main/java/org/quydusaigon/predatorsim/Prefabs.java
+++ src/main/java/org/quydusaigon/predatorsim/Prefabs.java
@@ -1,8 +1,17 @@
 package org.quydusaigon.predatorsim;
 
+import org.quydusaigon.predatorsim.gameengine.gameobject.GameObject;
+import org.quydusaigon.predatorsim.gameengine.util.TransformInit;
+
+import javafx.scene.Group;
+
 public final class Prefabs {
 
     private Prefabs() {
+    }
+
+    public static Group ROOT() {
+        return GameObject.create(TransformInit.ZERO, null);
     }
 
 }
