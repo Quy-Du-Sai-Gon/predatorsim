d5b8dda1a0fb4e399e304fdf3fcf65798717e5e7
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-12-23 16:55:23 +0100

    Add unfinished Particle

--- src/main/java/org/quydusaigon/predatorsim/util/Prefabs.java
+++ src/main/java/org/quydusaigon/predatorsim/util/Prefabs.java
@@ -1,9 +1,15 @@
 package org.quydusaigon.predatorsim.util;
 
+import org.quydusaigon.predatorsim.behaviours.Particle;
+import org.quydusaigon.predatorsim.gameengine.component.Collider;
+import org.quydusaigon.predatorsim.gameengine.component.NodeComponent;
 import org.quydusaigon.predatorsim.gameengine.gameobject.GameObject;
+import org.quydusaigon.predatorsim.gameengine.util.Prefab;
 import org.quydusaigon.predatorsim.gameengine.util.TransformInit;
 
 import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public final class Prefabs {
 
@@ -13,5 +19,10 @@ public final class Prefabs {
     public static Group ROOT() {
         return GameObject.create(TransformInit.DEFAULT, null);
     }
+
+    public static final Prefab PARTICLE = (tf, parent) -> {
+        var nodeComp = new NodeComponent<>(new Circle(0, Color.GREEN));
+        return GameObject.create(tf, parent, nodeComp, new Collider<>(nodeComp), new Particle());
+    };
 
 }
