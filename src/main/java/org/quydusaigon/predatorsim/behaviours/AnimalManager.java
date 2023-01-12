a005fdb77908a89354a35c9776b13711f0af76ba
Khang <khanglun20000@gmail.com>
2023-01-12 18:31:43 +0100

    Update AnimalManager.java

--- src/main/java/org/quydusaigon/predatorsim/behaviours/AnimalManager.java
+++ src/main/java/org/quydusaigon/predatorsim/behaviours/AnimalManager.java
@@ -2,10 +2,12 @@ package org.quydusaigon.predatorsim.behaviours;
 import org.quydusaigon.predatorsim.gameengine.component.Behaviour;
 import org.quydusaigon.predatorsim.gameengine.gameobject.GameObject;
 
+import java.util.*;
+
 public class AnimalManager extends Behaviour{
-//    private List<Animal> predatorList = new LinkedList<Animal>();
-//    private List<Animal> preyList = new LinkedList<Animal>();
-//
+    private List<Animal> predatorList = new ArrayList<>();
+    private List<Animal> preyList = new ArrayList<>();
+
 //    public void createAnimal(int predatorCount, int preyCount){
 //        for(int i = 0; i < predatorCount; i++){
 //            GameObject.instantiate();
