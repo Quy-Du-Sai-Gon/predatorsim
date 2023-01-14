1e0cd079411fd0ebe21ec92cb1074342db340cb0
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2023-01-14 23:46:43 +0100

    Change and add implementations

--- src/main/java/org/quydusaigon/predatorsim/behaviours/State.java
+++ src/main/java/org/quydusaigon/predatorsim/behaviours/State.java
@@ -1,11 +1,10 @@
 package org.quydusaigon.predatorsim.behaviours;
 
-import org.quydusaigon.predatorsim.behaviours.animalBehaviours.AnimalBehaviour;
 import org.quydusaigon.predatorsim.gameengine.component.Behaviour;
 
 public abstract class State extends Behaviour {
     protected Animal animalSM;
-    protected AnimalBehaviour animalBehaviour;
+
     public State(Animal animalSM) {
         this.animalSM = animalSM;
     }
--- src/main/java/org/quydusaigon/predatorsim/behaviours/states/WanderState.java
+++ src/main/java/org/quydusaigon/predatorsim/behaviours/states/WanderState.java
@@ -2,20 +2,25 @@ package org.quydusaigon.predatorsim.behaviours.states;
 
 import org.quydusaigon.predatorsim.behaviours.Animal;
 import org.quydusaigon.predatorsim.behaviours.State;
+import org.quydusaigon.predatorsim.behaviours.animalBehaviours.WanderBehaviour;
 
 public class WanderState extends State {
+    WanderBehaviour wanderBehaviour;
+
     public WanderState(Animal animalSM) {
         super(animalSM);
+        wanderBehaviour = animalSM.getWanderBehaviour();
     }
 
     @Override
     public void enter() {
+        wanderBehaviour.setSeed();
         super.enter();
     }
 
     @Override
     public void update() {
-
+        wanderBehaviour.doAction();
     }
 
     @Override
