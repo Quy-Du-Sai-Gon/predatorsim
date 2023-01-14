8958a0f091f7c289063751969741bf7280519431
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2023-01-14 23:47:50 +0100

    Implement wandering behavior

--- src/main/java/org/quydusaigon/predatorsim/behaviours/animalBehaviours/WanderBehaviour.java
+++ src/main/java/org/quydusaigon/predatorsim/behaviours/animalBehaviours/WanderBehaviour.java
@@ -1,11 +1,30 @@
 package org.quydusaigon.predatorsim.behaviours.animalBehaviours;
+import org.quydusaigon.predatorsim.util.PerlinNoise;
 
-import org.quydusaigon.predatorsim.behaviours.animalBehaviours.AnimalBehaviour;
+public class WanderBehaviour extends AnimalBehaviour {
 
-public abstract class WanderBehaviour extends AnimalBehaviour {
-    public abstract void doWander();
-    @Override
-    void doAction(){
-        doWander();
+    private double seedX;
+    private double seedY;
+
+    public void setSeed() {
+        seedX = Math.random() * 100;
+        seedY = Math.random() * 100;
     }
+
+    @Override
+    public void doAction(){
+        var x = posX();
+        var y = posY();
+
+        double randomx = PerlinNoise.noise(Math.PI, Math.E, seedX) * animalStat.runSpeed;
+        double randomy = PerlinNoise.noise(Math.PI, Math.E, seedY) * animalStat.runSpeed ;
+
+        seedX += 0.01;
+        seedY += 0.01;
+
+        x.set(x.get() + randomx);
+        y.set(y.get() + randomy);
+
+    }
+    
 }
