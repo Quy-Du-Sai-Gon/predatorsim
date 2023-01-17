a911436aa031994fe311b8f3873fffa54ddce926
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2023-01-17 23:19:26 +0100

    Update Level

--- src/main/java/org/quydusaigon/predatorsim/Level.java
+++ src/main/java/org/quydusaigon/predatorsim/Level.java
@@ -1,5 +1,6 @@
 package org.quydusaigon.predatorsim;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
@@ -9,37 +10,38 @@ import org.quydusaigon.predatorsim.gameengine.util.Helper;
 import org.quydusaigon.predatorsim.gameengine.util.Prefab;
 import org.quydusaigon.predatorsim.gameengine.util.TransformInit;
 import org.quydusaigon.predatorsim.util.Prefabs;
+import org.quydusaigon.predatorsim.UI;
 
 import javafx.scene.Group;
 
 public final class Level {
     private Level() {
     }
+    public static int numberOfPredator = 0;
+    public static int numberOfSmallPrey = 0;
+    public static int numberOfMediumPrey = 0;
+    public static int numberOfLargePrey = 0;
 
-    public static final Supplier<Group> main = Helper.lazyValue(() -> {
-        var root = Prefabs.ROOT();
-
-        int numberOfPredator = 1;
-        int numberOfSmallPrey = 5;
-        int numberOfMediumPrey = 5;
-        int numberOfLargePrey = 5;
-
+    public static void main() {
         IntStream.range(0, numberOfPredator).forEach((i) -> {
-            GameObject.instantiate(Prefabs.PREDATOR, TransformInit.getRandomTransformInit(), root);
+            GameObject.instantiate(Prefabs.PREDATOR, TransformInit.getRandomTransformInit());
         });
 
         IntStream.range(0, numberOfSmallPrey).forEach((i) -> {
-            GameObject.instantiate(Prefabs.SMALL_PREY, TransformInit.getRandomTransformInit(), root);
+            GameObject.instantiate(Prefabs.SMALL_PREY, TransformInit.getRandomTransformInit());
         });
 
         IntStream.range(0, numberOfMediumPrey).forEach((i) -> {
-            GameObject.instantiate(Prefabs.MEDIUM_PREY, TransformInit.getRandomTransformInit(), root);
+            GameObject.instantiate(Prefabs.MEDIUM_PREY, TransformInit.getRandomTransformInit());
         });
 
         IntStream.range(0, numberOfLargePrey).forEach((i) -> {
-            GameObject.instantiate(Prefabs.LARGE_PREY, TransformInit.getRandomTransformInit(), root);
+            GameObject.instantiate(Prefabs.LARGE_PREY, TransformInit.getRandomTransformInit());
         });
+    };
 
-        return root;
-    });
+    public static void changeAnimalNumber(int predatorNumber, int preyNumber) {
+        numberOfPredator = predatorNumber;
+        numberOfLargePrey = preyNumber;
+    }
 }
