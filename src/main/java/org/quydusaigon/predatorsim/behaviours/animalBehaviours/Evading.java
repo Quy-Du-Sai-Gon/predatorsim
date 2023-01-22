763dff62cfd8c6ca41e9d28233d45da0a55a1cda
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2023-01-22 23:46:31 +0100

    Add random movement to evading
    Co-Authored-By: khanglun20000 <66657823+khanglun20000@users.noreply.github.com>

--- src/main/java/org/quydusaigon/predatorsim/behaviours/animalBehaviours/Evading.java
+++ src/main/java/org/quydusaigon/predatorsim/behaviours/animalBehaviours/Evading.java
@@ -1,8 +1,20 @@
 package org.quydusaigon.predatorsim.behaviours.animalBehaviours;
 
-public class Evading extends SurvivalBehaviour{
+import java.util.ArrayList;
+
+import javafx.scene.Group;
+
+public class Evading extends SurvivalBehaviour {
+    private ArrayList<Group> targetObject;
+
     @Override
     public void doSurvival() {
+        var thisX = posX();
+        var thisY = posY();
+    }
 
+    @Override
+    public void setTargetObject(Group object) {
+        targetObject.add(object);
     }
 }
