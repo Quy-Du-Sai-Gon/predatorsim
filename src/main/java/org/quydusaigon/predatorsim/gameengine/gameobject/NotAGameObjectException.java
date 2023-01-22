9487a19e439527888c36e17aeaf0779ea22c8a92
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-01-22 18:57:41 +0100

    Add javadoc for NotAGameObjectException

--- src/main/java/org/quydusaigon/predatorsim/gameengine/gameobject/NotAGameObjectException.java
+++ src/main/java/org/quydusaigon/predatorsim/gameengine/gameobject/NotAGameObjectException.java
@@ -1,5 +1,12 @@
 package org.quydusaigon.predatorsim.gameengine.gameobject;
 
+/**
+ * A runtime exception thrown when {@link GameObject} operations are called on
+ * invalid game objects ({@link javafx.scene.Group}s that are not created with
+ * {@link GameObject#create}).
+ * 
+ * @see GameObject
+ */
 public class NotAGameObjectException extends RuntimeException {
 
 }
