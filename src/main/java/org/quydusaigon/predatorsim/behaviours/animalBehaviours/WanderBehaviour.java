e31ba23e27b0340e1847065ff44cbf4be862db03
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2023-01-23 06:30:12 +0100

    Improve Wandering algorithm

--- src/main/java/org/quydusaigon/predatorsim/behaviours/animalBehaviours/WanderBehaviour.java
+++ src/main/java/org/quydusaigon/predatorsim/behaviours/animalBehaviours/WanderBehaviour.java
@@ -1,7 +1,17 @@
 package org.quydusaigon.predatorsim.behaviours.animalBehaviours;
 
+import org.quydusaigon.predatorsim.App;
+import org.quydusaigon.predatorsim.behaviours.Animal;
 import org.quydusaigon.predatorsim.gameengine.Time;
+import org.quydusaigon.predatorsim.gameengine.gameobject.GameObject;
 import org.quydusaigon.predatorsim.util.PerlinNoise;
+import org.quydusaigon.predatorsim.util.PredatorStat;
+import org.quydusaigon.predatorsim.util.PreySize;
+import org.quydusaigon.predatorsim.util.PreyStat;
+
+import javafx.scene.Group;
+
+import org.quydusaigon.predatorsim.util.AnimalStat;
 import org.quydusaigon.predatorsim.util.Map;
 
 public class WanderBehaviour extends AnimalBehaviour {
@@ -19,11 +29,36 @@ public class WanderBehaviour extends AnimalBehaviour {
         var x = posX();
         var y = posY();
 
-        double randomX = PerlinNoise.noise(Math.PI, Math.E, seedX) * animalStat.runSpeed * Time.getDeltaTime() * 100;
-        double randomY = PerlinNoise.noise(Math.PI, Math.E, seedY) * animalStat.runSpeed * Time.getDeltaTime() * 100;
+        double randomX = PerlinNoise.noise(Math.PI, Math.E, seedX) * animalStat.runSpeed/2 * Time.getDeltaTime() * 100;
+        double randomY = PerlinNoise.noise(Math.PI, Math.E, seedY) * animalStat.runSpeed/2 * Time.getDeltaTime() * 100;
 
-        seedX += 0.005;
-        seedY += 0.005;
+        
+        Group object = getGameObject();
+
+        AnimalStat stat = GameObject.getComponent(object, Animal.class).get().animalStat;
+        if (stat instanceof PreyStat) {
+            var preyStat = (PreyStat) stat;
+            if (preyStat.size == PreySize.SMALL) {
+                seedX += 0.009;
+            }
+            else if (preyStat.size == PreySize.MEDIUM) {
+                seedX += 0.007;
+            }
+            if (preyStat.size == PreySize.LARGE) {
+                seedX += 0.003;
+            }
+        }
+
+        else if (stat instanceof PredatorStat) {
+            seedX += 0.01;
+            seedY += 0.01;
+        }
+ 
+        double mapTendencyOffsetX = Math.abs(App.simulationWindowWidth - posX().get());
+        double mapTendencyOffsetY =  Math.abs(App.simulationWindowHeight - posY().get());
+
+        seedX += 0.01 - mapTendencyOffsetX/50000;
+        seedY += 0.01 - mapTendencyOffsetY/50000;
 
         x.set(Map.checkBoundX(posX().get() + randomX));
         y.set(Map.checkBoundY(posY().get() + randomY));
