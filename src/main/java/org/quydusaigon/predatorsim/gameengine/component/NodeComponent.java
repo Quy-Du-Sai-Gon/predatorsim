be4e375074adbb521c5f681c0751b2725626ac24
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-01-23 13:03:44 +0100

    Add javadoc for NodeComponent

--- src/main/java/org/quydusaigon/predatorsim/gameengine/component/NodeComponent.java
+++ src/main/java/org/quydusaigon/predatorsim/gameengine/component/NodeComponent.java
@@ -1,17 +1,47 @@
 package org.quydusaigon.predatorsim.gameengine.component;
 
+import org.quydusaigon.predatorsim.gameengine.gameobject.GameObject;
+
 import javafx.scene.Node;
 
+/**
+ * A {@link Component} that wraps around a {@link javafx.scene.Node} in the
+ * scene. The {@link Node} this wraps around is automatically added as a child
+ * of the game object (represented by a {@link Group}) that this component is
+ * added to.
+ *
+ * <p>
+ * Can be considered as a <i>visual component</i> for the scene.
+ * 
+ * @see Component
+ * @see Node
+ * @see GameObject
+ */
 public class NodeComponent<T extends Node> extends Component {
     private final T node;
     private Collider<T> collider;
 
+    /**
+     * Creates a new {@link NodeComponent} that wraps around a {@link Node}. The
+     * {@link Node} this wraps around is automatically added as a child of the game
+     * object (represented by a {@link Group}) that this component is added to.
+     * 
+     * @param node the {@link Node} to wrap.
+     * @see Node
+     * @see GameObject
+     */
     public NodeComponent(T node) {
         if (node == null)
             throw new NullPointerException("Node cannot be null");
         this.node = node;
     }
 
+    /**
+     * Returns the {@link javafx.scene.Node} that this {@link NodeComponent} wraps.
+     * 
+     * @return the {@link javafx.scene.Node} that this {@link NodeComponent} wraps.
+     * @see Node
+     */
     public T getNode() {
         return node;
     }
