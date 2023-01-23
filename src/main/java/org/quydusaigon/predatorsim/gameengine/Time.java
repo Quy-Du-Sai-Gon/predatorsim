e1335fd77b5920764b5eda59c6e93b2e32123275
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-01-24 00:27:35 +0100

    Add javadoc for Time

--- src/main/java/org/quydusaigon/predatorsim/gameengine/Time.java
+++ src/main/java/org/quydusaigon/predatorsim/gameengine/Time.java
@@ -1,17 +1,35 @@
 package org.quydusaigon.predatorsim.gameengine;
 
 import org.quydusaigon.predatorsim.App;
+import org.quydusaigon.predatorsim.gameengine.component.Behaviour;
 
+/**
+ * This class provides static methods to access data about time. This is useful
+ * for {@link Behaviour} scripts that needs information about time between each
+ * frame.
+ */
 public class Time {
 
     private static float deltaTime;
     private static long prevTimeNs;
 
-    public static void reset() {
+    /**
+     * Resets the internal state of the time information system to avoid the system
+     * being left in a bad and stale state due to insufficient internal update by
+     * the {@link GameLoop}; i.e., when the game loop is first started or after the
+     * game loop has been stopped for a while and restarted again.
+     */
+    static void reset() {
         deltaTime = 0;
         prevTimeNs = -1;
     }
 
+    /**
+     * Updates the internal time information with new data.
+     * 
+     * @param nowNs new current timestamp in nanoseconds or {@code -1} to indicate a
+     *              move in {@linkplain App#getTimeStep() time step}.
+     */
     static void update(long nowNs) {
         if (nowNs == -1) {
             // for fixed time step
@@ -26,6 +44,19 @@ public class Time {
         prevTimeNs = nowNs;
     }
 
+    /**
+     * Returns the elapsed time in seconds between the current frame and the
+     * previous frame while the game loop is running.
+     *
+     * <p>
+     * Alternatively, returns {@link App#getTimeStep()} if the last time the
+     * {@code Time} system was updated was with the fixed time step specified. See
+     * {@link GameLoop#handle(long)}.
+     * 
+     * @return the elapsed time in seconds between the current frame and the
+     *         previous frame.
+     * @see GameLoop
+     */
     public static float getDeltaTime() {
         return deltaTime;
     }
