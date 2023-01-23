a4e5c57c673d94a6f8bbbabb319a76e883195a5f
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-01-23 12:35:33 +0100

    Add javadoc for Collider

--- src/main/java/org/quydusaigon/predatorsim/gameengine/component/Collider.java
+++ src/main/java/org/quydusaigon/predatorsim/gameengine/component/Collider.java
@@ -4,10 +4,27 @@ import javafx.scene.Node;
 import javafx.scene.shape.Shape;
 import javafx.geometry.Bounds;
 
+/**
+ * A {@link Component} acting as a physical node in the scene that can collides
+ * with other {@code Collider}s. This class wraps around a
+ * {@link NodeComponent}, enabling collision detection for certain {@link Node}s
+ * in the scene.
+ * 
+ * @see Component
+ * @see NodeComponent
+ */
 public class Collider<T extends Node> extends Component {
 
     private final NodeComponent<T> nodeComponent;
 
+    /**
+     * Creates a new collider that wraps around {@code nodeComponent}, enabling
+     * collision detection for that {@link NodeComponent}.
+     * 
+     * @param nodeComponent the {@link NodeComponent} to wrap and enable collision
+     *                      for.
+     * @see NodeComponent
+     */
     public Collider(NodeComponent<T> nodeComponent) {
         this.nodeComponent = nodeComponent;
         nodeComponent.setCollider(this);
@@ -17,10 +34,26 @@ public class Collider<T extends Node> extends Component {
      * NodeComponent access
      */
 
+    /**
+     * Returns the {@link NodeComponent} that this collider wraps.
+     * 
+     * @return the {@link NodeComponent} that this collider wraps.
+     * @see NodeComponent
+     */
     public NodeComponent<T> getNodeComponent() {
         return nodeComponent;
     }
 
+    /**
+     * Returns the {@link javafx.scene.Node} wrapped by the {@link NodeComponent}
+     * that this collider wraps. Short-hand for
+     * {@code getNodeComponent().getNode()}.
+     * 
+     * @return the {@link javafx.scene.Node} wrapped by the {@link NodeComponent}
+     *         that this collider wraps.
+     * @see NodeComponent
+     * @see Node
+     */
     public T getNode() {
         return getNodeComponent().getNode();
     }
@@ -34,11 +67,30 @@ public class Collider<T extends Node> extends Component {
      * Collision detection
      */
 
+    /**
+     * Checks whether this collider collides with a global {@link Bounds}.
+     * Essentially checking whether the underlying {@link Node} of this collider
+     * intersects with {@code globalBounds}.
+     * 
+     * @param globalBounds the {@link Bounds} to check for.
+     * @return whether this collider collides with a global {@link Bounds}.
+     * @see Node
+     * @see Bounds
+     */
     public boolean collides(Bounds globalBounds) {
         var node = getNode();
         return node.intersects(node.sceneToLocal(globalBounds));
     }
 
+    /**
+     * Checks whether this collider collides with a {@link Node}. Essentially
+     * checking whether the underlying {@link Node} of this collider intersects with
+     * {@code node}.
+     * 
+     * @param node the {@link Node} to check for.
+     * @return whether this collider collides with a {@link Node}.
+     * @see Node
+     */
     public boolean collides(Node node) {
         var thisNode = getNode();
         if (thisNode instanceof Shape && node instanceof Shape) {
@@ -54,6 +106,15 @@ public class Collider<T extends Node> extends Component {
         return thisNode.intersects(getBoundsLocalTo(node, thisNode));
     }
 
+    /**
+     * Checks whether this collider collides with another collider. Essentially
+     * checking whether the underlying {@link Node} of this collider intersects with
+     * the underlying {@link Node} of the {@code other} collider.
+     * 
+     * @param other the other collider to check for.
+     * @return whether this collider collides with another collider.
+     * @see Node
+     */
     public boolean collides(Collider<?> other) {
         return collides(other.getNode());
     }
