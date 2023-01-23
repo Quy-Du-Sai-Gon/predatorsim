ed88a661c3b8a42cc1944f5231e0d1478cda213d
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2023-01-23 01:35:41 +0100

    Add a parameter class for the game default values

---
+++ src/main/java/org/quydusaigon/predatorsim/util/Parameter.java
@@ -0,0 +1,42 @@
+package org.quydusaigon.predatorsim.util;
+
+public class Parameter {
+    public static final boolean DEFAULT_SHOW_VISION = true;
+    public static final boolean DEFAULT_SHOW_OBJECT_STAT = false;
+
+    public static final double DEFAULT_PREDATOR_SPEED_MINIMUM_RANGE = 5;
+    public static final double DEFAULT_PREDATOR_SPEED_MAXIMUM_RANGE = 6;
+    public static final double DEFAULT_SMALL_PREY_SPEED_MINIMUM_RANGE = 4;
+    public static final double DEFAULT_SMALL_PREY_SPEED_MAXIMUM_RANGE = 5;
+    public static final double DEFAULT_MEDIUM_PREY_SPEED_MINIMUM_RANGE = 3;
+    public static final double DEFAULT_MEDIUM_PREY_SPEED_MAXIMUM_RANGE = 4;
+    public static final double DEFAULT_LARGE_PREY_SPEED_MINIMUM_RANGE = 2;
+    public static final double DEFAULT_LARGE_PREY_SPEED_MAXIMUM_RANGE = 3;
+
+    public static final double DEFAULT_PREDATOR_VISION_MINIMUM_RANGE = 80;
+    public static final double DEFAULT_PREDATOR_VISION_MAXIMUM_RANGE = 110;
+    public static final double DEFAULT_SMALL_PREY_VISION_MINIMUM_RANGE = 70;
+    public static final double DEFAULT_SMALL_PREY_VISION_MAXIMUM_RANGE = 90;
+    public static final double DEFAULT_MEDIUM_PREY_VISION_MINIMUM_RANGE = 70;
+    public static final double DEFAULT_MEDIUM_PREY_VISION_MAXIMUM_RANGE = 90;
+    public static final double DEFAULT_LARGE_PREY_VISION_MINIMUM_RANGE = 70;
+    public static final double DEFAULT_LARGE_PREY_VISION_MAXIMUM_RANGE = 90;
+
+    public static final int DEFAULT_SMALL_PREY_NUTRITION_MINIMUM_RANGE = 15;
+    public static final int DEFAULT_SMALL_PREY_NUTRITION_MAXIMUM_RANGE = 25;
+    public static final int DEFAULT_MEDIUM_PREY_NUTRITION_MINIMUM_RANGE = 50;
+    public static final int DEFAULT_MEDIUM_PREY_NUTRITION_MAXIMUM_RANGE = 65;
+    public static final int DEFAULT_LARGE_PREY_NUTRITION_MINIMUM_RANGE = 100;
+    public static final int DEFAULT_LARGE_PREY_NUTRITION_MAXIMUM_RANGE = 120;
+
+    public static final double DEFAULT_SMALL_PREY_DEFENSE_MINIMUM_RANGE = 0;
+    public static final double DEFAULT_SMALL_PREY_DEFENSE_MAXIMUM_RANGE = 10;
+    public static final double DEFAULT_MEDIUM_PREY_DEFENSE_MINIMUM_RANGE = 30;
+    public static final double DEFAULT_MEDIUM_PREY_DEFENSE_MAXIMUM_RANGE = 50;
+    public static final double DEFAULT_LARGE_PREY_DEFENSE_MINIMUM_RANGE = 75;
+    public static final double DEFAULT_LARGE_PREY_DEFENSE_MAXIMUM_RANGE = 100;
+
+    public static final int DEFAULT_PREDATOR_STARVATION_RESILLIENCE_MINIMUM_RANGE = 30;
+    public static final int DEFAULT_PREDATOR_STARVATION_RESILLIENCE_MAXIMUM_RANGE = 50;
+    public static final double DEFAULT_PREDATOR_GROUP_RADIUS = 100;
+}
