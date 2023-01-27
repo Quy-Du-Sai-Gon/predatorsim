3f5b8cf47eceb5cbe7b2e57eb3f6d320cb4b511d
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2023-01-28 00:41:34 +0100

    Add implementation for hunting in group

--- src/main/java/org/quydusaigon/predatorsim/behaviours/animalBehaviours/HuntingInGroup.java
+++ src/main/java/org/quydusaigon/predatorsim/behaviours/animalBehaviours/HuntingInGroup.java
@@ -1,24 +1,86 @@
 package org.quydusaigon.predatorsim.behaviours.animalBehaviours;
 
 import java.util.ArrayList;
+import java.util.Optional;
+
+import org.quydusaigon.predatorsim.gameengine.component.Collider;
+import org.quydusaigon.predatorsim.gameengine.component.NodeComponent;
+import org.quydusaigon.predatorsim.gameengine.gameobject.GameObject;
+import org.quydusaigon.predatorsim.gameengine.util.TransformInit;
+import org.quydusaigon.predatorsim.util.PredatorStat;
 
 import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class HuntingInGroup extends Hunting {
     private ArrayList<Group> alliesObjects;
+    private Optional<Group> leaderObject;
+    private int numberOfAllies;
+    private boolean isLeader = false;
+    private boolean groupFounded = false;
 
     public void doSurvival() {
-        var x = posX();
-        var y = posY();
+        if (groupFounded) {
+            if (isLeader) {
+                leaderChase();
+                return;
+            }
+            else {
+                memberChase();
+                return;
+            }
+        }
+        else {
+            if (isLeader) {
+                howl();
+            }
+            else {
+                join();
+            }
+        }
     }
-//
-//    @Override
-//    public void setTargetObject(Group object) {
-//        targetObject = object;
-//    }
-//
-//    @Override
-//    public void setAlliesObject(Group object) {
-//        alliesObjects.add(object);
-//    }
+
+    public void setNumberOfAllies(int numberOfAllies) {
+        this.numberOfAllies = numberOfAllies;
+    }
+
+    public void setIsLeader(boolean isLeader) {
+        this.isLeader = isLeader;
+    }
+
+    public void setLeader(Group gameObject) {
+        leaderObject = Optional.of(gameObject);
+    }
+
+    public void startHowling() {
+        this.isLeader = true;
+        PredatorStat predatorStat = (PredatorStat) animalStat;
+
+        var circleHowling = new Circle(predatorStat.howlingRadius, Color.DARKCYAN);
+        circleHowling.setOpacity(0.4);
+        var howlingVisionNodeComp = new NodeComponent<>(circleHowling);
+        Collider howlCollider = new Collider<>(howlingVisionNodeComp);
+
+        var circleGroup = new Circle(predatorStat.groupRadius, Color.DARKRED);
+        circleGroup.setOpacity(0.4);
+        var groupVisionNodeComp = new NodeComponent<>(circleGroup);
+        Collider groupCollider = new Collider<>(groupVisionNodeComp);
+    }
+
+    public void howl() {
+        
+    }
+
+    public void join() {
+    
+    }
+
+    public void leaderChase() {
+
+    }
+
+    public void memberChase() {
+
+    }
 }
